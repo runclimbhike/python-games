b25ce4d6c73c798f56397da1bec133f85ac83540
Kevin Sibley <kevin.sibley@kohls.com>
2019-06-13 14:54:24 -0500

    - finished coin collector. working

--- coin-collector/coin.py
+++ coin-collector/coin.py
@@ -1,5 +1,6 @@
 # IMPORTS
 import pgzrun
+from random import randint
 
 # VARIABLES
 WIDTH = 400
@@ -20,6 +21,39 @@ def draw():
     fox.draw()
     coin.draw()
     screen.draw.text("Score: " + str(score), color="black", topleft=(10, 10))
+
+    if game_over:
+        screen.fill("pink")
+        screen.draw.text("Final Score: " + str(score), topleft=(10, 10), fontsize=60)
+
+def place_coin():
+    coin.x = randint(20, (WIDTH - 20))
+    coin.y = randint(20, (HEIGHT - 20))
+
+def time_up():
+    global game_over
+    game_over = True
+
+def update():
+    global score
+
+    if keyboard.left:
+        fox.x = fox.x - 4
+    elif keyboard.right:
+        fox.x = fox.x + 4
+    elif keyboard.up:
+        fox.y = fox.y - 4
+    elif keyboard.down:
+        fox.y = fox.y + 4
+
+    coin_collected = fox.colliderect(coin)
+
+    if coin_collected:
+        score = score + 10
+
+        place_coin()
+
+clock.schedule(time_up, 7.0)
 
 
 pgzrun.go()
