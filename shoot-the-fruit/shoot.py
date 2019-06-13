da24bd14b001017e887074fca16673974b657c90
Kevin Sibley <kevin.sibley@kohls.com>
2019-06-13 10:31:11 -0500

    added place_apple function.

--- shoot-the-fruit/shoot.py
+++ shoot-the-fruit/shoot.py
@@ -6,9 +6,18 @@ import pgzrun
 
 apple = Actor("apple")
 
+
 def draw():
     screen.clear()
     apple.draw()
 
-pgzrun.go()
 
+def place_apple():
+    apple.x = 300
+    apple.y = 200
+
+
+place_apple()
+
+
+pgzrun.go()
