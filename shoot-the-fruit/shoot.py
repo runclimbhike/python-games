19c9890c74abf581a9084a84fbeb588a6aa7a5a2
Kevin Sibley <kevin.sibley@kohls.com>
2019-06-13 10:49:40 -0500

    completed the functions and the game actually works!!!

--- shoot-the-fruit/shoot.py
+++ shoot-the-fruit/shoot.py
@@ -1,9 +1,12 @@
 # Date: 2019-06-13
 # more comments to make a commit
 
+# IMPORTS
 from pgzrun import *
 import pgzrun
+from random import randint
 
+# VARIABLES
 apple = Actor("apple")
 
 
@@ -13,8 +16,17 @@ def draw():
 
 
 def place_apple():
-    apple.x = 300
-    apple.y = 200
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 800)
+
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Good shot!")
+        place_apple()
+    else:
+        print("You missed!")
+        quit()
 
 
 place_apple()
