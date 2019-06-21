fa39742e0ada87279801d597d0c3e0ac6c34769d
Kevin Sibley <kevin.sibley@kohls.com>
2019-06-21 13:55:33 -0500

    - made the map bigger - made the fox move faster - made the time to play longer

--- coin-collector/coin.py
+++ coin-collector/coin.py
@@ -16,6 +16,7 @@ fox.pos = 100, 100
 coin = Actor("coin")
 coin.pos = 200, 200
 
+
 def draw():
     screen.fill("green")
     fox.draw()
@@ -26,25 +27,28 @@ def draw():
         screen.fill("pink")
         screen.draw.text("Final Score: " + str(score), topleft=(10, 10), fontsize=60)
 
+
 def place_coin():
     coin.x = randint(20, (WIDTH - 20))
     coin.y = randint(20, (HEIGHT - 20))
+
 
 def time_up():
     global game_over
     game_over = True
 
+
 def update():
     global score
 
     if keyboard.left:
-        fox.x = fox.x - 10
+        fox.x = fox.x - 15
     elif keyboard.right:
-        fox.x = fox.x + 10
+        fox.x = fox.x + 15
     elif keyboard.up:
-        fox.y = fox.y - 10
+        fox.y = fox.y - 15
     elif keyboard.down:
-        fox.y = fox.y + 10
+        fox.y = fox.y + 15
 
     coin_collected = fox.colliderect(coin)
 
@@ -54,7 +58,7 @@ def update():
         place_coin()
 
 
-clock.schedule(time_up, 15.0)
+clock.schedule(time_up, 30.0)
 
 
 pgzrun.go()
